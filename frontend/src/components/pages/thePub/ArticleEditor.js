afffda5cc3ada67f07d051df72d4184ea09ddecc
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-04-09 18:44:23 +0300

    Finished emoticons and format buttons for the Article Editor. Only the table button needs to be thought out.

--- frontend/src/components/pages/thePub/ArticleEditor.js
+++ frontend/src/components/pages/thePub/ArticleEditor.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import { FaBold } from "react-icons/fa";
 import { FaItalic } from "react-icons/fa";
-import { FaAlignCenter } from "react-icons/fa";
-import { FaAlignLeft } from "react-icons/fa";
-import { FaAlignRight } from "react-icons/fa";
-import { FaUndo } from "react-icons/fa";
+import { FaImage } from "react-icons/fa";
+import { FaLink } from "react-icons/fa";
+import { FaHeading } from "react-icons/fa";
+import { FaUnderline } from "react-icons/fa";
+import { FaQuoteRight } from "react-icons/fa";
+import { FaTable } from "react-icons/fa";
 
 export default class ArticleEditor extends Component {
     constructor(props) {
@@ -13,6 +15,42 @@ export default class ArticleEditor extends Component {
             text: '',
             title:'',
             requestMsg:''
+        }
+    }
+
+    componentDidMount(){
+        document.querySelectorAll("button.emote").forEach((el) => {
+            el.addEventListener("click", (e) => {
+                emoteInTextarea(document.getElementById("Description"), e.currentTarget.value);
+            });
+        });
+
+        function emoteInTextarea(el, newText) {
+            var start = el.selectionStart;
+            var end = el.selectionEnd;
+            var text = el.value;
+            var before = text.substring(0, start);
+            var after = text.substring(end, text.length);
+            el.value = (before + newText + after);
+            el.selectionEnd = start + newText.length;
+            el.focus();
+        }
+
+        document.querySelectorAll("button.text").forEach((el) => {
+            el.addEventListener("click", (e) => {
+                typeInTextarea(document.getElementById("Description"), e.currentTarget.value);
+            });
+        });
+
+        function typeInTextarea(el, newText) {
+            var start = el.selectionStart;
+            var end = el.selectionEnd;
+            var text = el.value;
+            var before = text.substring(0, start);
+            var after = text.substring(end, text.length);
+            el.value = (before + newText + after);
+            el.selectionEnd = start + (newText.indexOf("]") + 1);
+            el.focus();
         }
     }
 
@@ -35,25 +73,30 @@ export default class ArticleEditor extends Component {
     render() {
 
         return (
-            <div>
-                <form className="text-center mx-auto">
-                    <h5>{this.state.requestMsg}</h5>
-                    <div className="m-20">
-                        <input placeholder={'title'} value={this.state.title} onChange={(e) => this.setState({title:e.target.value})}/><br/>
-                    </div>
-                    <div>
-                        <button><FaBold/></button>
-                        <button><FaItalic/></button>
-                        <button><FaAlignLeft/></button>
-                        <button><FaAlignCenter/></button>
-                        <button><FaAlignRight/></button>
-                        <button><FaUndo/></button>
-                    </div>
-                    <div className="m-20">
-                        <textarea cols="60" rows="8" onChange={(e) => this.setState({text:e.target.value})} value={this.state.text} placeholder={'write your articles here'}/><br/>
-                    </div>
-                    <button onClick={this.postArticle.bind(this)}>Submit</button>
-                </form>
+            <div className="text-center">
+                <h5>{this.state.requestMsg}</h5>
+                <div className="m-20">
+                    <input placeholder={'title'} value={this.state.title} onChange={(e) => this.setState({title:e.target.value})}/><br/>
+                </div>
+                <div>
+                    <button className="text" title="bold" value="[b][/b]"><FaBold/></button>
+                    <button className="text" title="italic" value="[i][/i]"><FaItalic/></button>
+                    <button className="text" title="heading" value="[h2][/h2]"><FaHeading/></button>
+                    <button className="text" title="underline" value="[u][/u]"><FaUnderline/></button>
+                    <button className="text" title="quote" value="[quote][/quote]"><FaQuoteRight/></button>
+                    <button className="text" title="link" value="[link=URL][/link]"><FaLink/></button>
+                    <button className="text" title="image" value="[img=URL][/img]"><FaImage/></button>
+                    {/* table needs more thinking */}
+                    <button className="text" title="table" value="[table][/table]"><FaTable/></button>
+                    <button className="emote" value="😃">😃</button>
+                    <button className="emote" value="🙂">🙂</button>
+                    <button className="emote" value="☹">☹</button>
+                    <button className="emote" value="😮">😮</button>
+                </div>
+                <div className="m-20">
+                <textarea id="Description" cols="60" rows="8" onChange={(e) => this.setState({text:e.target.value})} value={this.state.text} placeholder={'write your articles here'}/><br/>
+                </div>
+                <button onClick={this.postArticle.bind(this)}>Submit</button>
             </div>
         );
     }
